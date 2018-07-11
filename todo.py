b663dc6f66193aeb6cc0022684ff50cfe91963e2
rajansaini980 <rajansaini980@gmail.com>
2018-07-11 11:01:45 +0530

    final submission commit

--- todo.py
+++ todo.py
@@ -1,6 +1,6 @@
 # rajan todo list programme
 from tkinter import *
-
+from tkinter import messagebox
 import sys          #System Module For Exit Function
 import random
 import tkinter.messagebox as msg
@@ -40,6 +40,7 @@ def add_task():
     task=entry.get("1.0",END)
     task_list.append(task)
     update_mylist()
+
 
 
 #Method for deleting all the tasks.
@@ -99,20 +100,24 @@ def num_of_task():
 
 #Method to exit from the root window.
 def exit():
-    sys.exit()
+    result=messagebox.askyesno('exit','Are you sure want to exit')
+    if result==True:
+        sys.exit()
+    else:
+        pass
 
 
 #Label for printing -->>To-Do List<<-- on root wimdow.
-label=Label(root,text="To-Do List",width="10")
-label.place(x=42,y=30)
+label=Label(root,text="To-Do List",width="9",font='10')
+label.place(x=42,y=25)
 
 
 
 #Entry for input as here user will enter the task to add in task_list(ListBox).
-entry=Text(root,width=15,height="1")
+entry=Text(root,width=30,height="1")
 # entry.insert("end",'Enter your task')
 # entry.bind("<Button>",Add_Task)
-entry.place(x=42,y=50)
+entry.place(x=42,y=55)
 
 
 #ListBox for keeping the all tasks.
@@ -122,7 +127,7 @@ mylist.place(x=42,y=100)
 
 #Button for adding new tasks in task_list(ListBox).
 add_item_button=Button(root,text="Add to-do item",command=add_task,width=15)
-add_item_button.place(x=435,y=142)
+add_item_button.place(x=435,y=170)
 
 
 #Button for deleting all tasks from task_list(ListBox).
@@ -132,28 +137,28 @@ clr_task_button.place(x=435,y=200)
 
 #Button for deleting one task from task_list(ListBox).
 remove_button=Button(root,text="Remove",command=remove,width=15)
-remove_button.place(x=200,y=180)
+remove_button.place(x=435,y=230)
 
 
 #Button to sort the tasks in ascending order.
 asc_sort_button=Button(root,text="Ascending sort",command=asc_order,width=15)
-asc_sort_button.place(x=200,y=220)
+asc_sort_button.place(x=435,y=260)
 
 
 #Button to sort the tasks in descending order.
 sort_desc_button=Button(root,text="Sort(DESC)",command=desc_order,width=15)
-sort_desc_button.place(x=200,y=260)
+sort_desc_button.place(x=435,y=290)
 
 
 #Button to choose a random task from the task_list(ListBox).
 choose_random_button=Button(root,text="Choose Random",command=choose_random,width=15)
-choose_random_button.place(x=200,y=300)
+choose_random_button.place(x=435,y=320)
 #Button to calculate the number of tasks in task_list(ListBox).
-number_of_tasks_button=Button(root,text="Number of Tasks",command=num_of_task,width=15,font="5",bg="yellow")
-number_of_tasks_button.place(x=200,y=340)
+number_of_tasks_button=Button(root,text="Number of Tasks",command=num_of_task,width=15)
+number_of_tasks_button.place(x=435,y=350)
 #Button for exiting.
-exit_button=Button(root,text="Exit",font=5,command=exit,width=8,bg="red")  #For Exit above defined exit function is not needed here sys module invokes the exit function with only command=exit or "exit"
-exit_button.place(x=460,y=520)
+exit_button=Button(root,text="Exit",font=5,command=exit,width=8,bg="powder blue")  #For Exit above defined exit function is not needed here sys module invokes the exit function with only command=exit or "exit"
+exit_button.place(x=440,y=420)
 #OR-->>Instructions for exit function
 # exit_button=Button(root,text="Exit",command=exit)
 # exit_button=Button(root,text="Exit",command="exit")
